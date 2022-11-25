9d4ea9ebfa1b97c725cf06b16dfea0c85bf8e2f1
Valdir Silva <valdir.ti@gmail.com>
2022-11-24 21:46:22 -0300

    feat: config eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from "express"
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'API is working' })
+app.get("/", (req, res) => {
+  return res.json({ message: "API is working" })
 })
 
-app.listen(3333)
+app.listen(3333, () => console.log("Server is running!"))

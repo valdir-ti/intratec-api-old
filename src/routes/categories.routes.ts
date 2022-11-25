96cee1598f42350fdbac8e2c1458c300b7c90c25
Valdir Silva <valdir.ti@gmail.com>
2022-11-24 22:52:27 -0300

    feat: add type to categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,20 @@
 import { Router } from "express"
-import { v4 as uuidV4 } from "uuid"
+
+import { Category } from "../model/Category"
 
 const categoriesRoutes = Router()
 
-const categories = []
+const categories: Category[] = []
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body
 
-  const category = {
-    id: uuidV4,
+  const category = new Category()
+  Object.assign(category, {
     name,
     description,
-  }
+    created_at: new Date(),
+  })
 
   categories.push(category)
 

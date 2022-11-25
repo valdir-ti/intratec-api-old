d1cdb861ea183263437dbb6a8c61ef7eda109a10
Valdir Silva <valdir.ti@gmail.com>
2022-11-24 22:26:12 -0300

    chore: config debbuger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
-import express from "express"
+import express, { response } from "express"
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   return res.json({ message: "API is working" })
+})
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body
+
+  return res.json({ name })
 })
 
 app.listen(3333, () => console.log("Server is running!"))

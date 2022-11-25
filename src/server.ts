529a4631bdffb58448953792514b5d62611bee60
Valdir Silva <valdir.ti@gmail.com>
2022-11-24 22:42:06 -0300

    feat: create category with uuid

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
-import express, { response } from "express"
+import express from "express"
+
+import { categoriesRoutes } from "./routes"
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
+app.use("/categories", categoriesRoutes)
+
+app.get("/", (_, res) => {
   return res.json({ message: "API is working" })
-})
-
-app.post("/courses", (req, res) => {
-  const { name } = req.body
-
-  return res.json({ name })
 })
 
 app.listen(3333, () => console.log("Server is running!"))

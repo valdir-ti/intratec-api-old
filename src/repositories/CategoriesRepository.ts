2314e834d2e5927eadbf5c3a364110904679e52b
Valdir Silva <valdir.ti@gmail.com>
2022-11-28 18:45:17 -0300

    feat: check if category exists

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,31 +1,36 @@
-import { Category } from '../model/Category'
+import { Category } from "../model/Category"
 
 interface ICreateCategoryDTO {
-    name: string
-    description: string
+  name: string
+  description: string
 }
 
 class CategoriesRepository {
-    private categories: Category[]
+  private categories: Category[]
 
-    constructor() {
-        this.categories = []
-    }
+  constructor() {
+    this.categories = []
+  }
 
-    create ({name, description}: ICreateCategoryDTO): void {
-        const category = new Category()
-        Object.assign(category, {
-            name,
-            description,
-            created_at: new Date(),
-        })
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category()
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    })
 
-        this.categories.push(category)
-    }
+    this.categories.push(category)
+  }
 
-    list (): Category[] {
-        return this.categories
-    }
+  list(): Category[] {
+    return this.categories
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find(category => category.name === name)
+    return category
+  }
 }
 
 export { CategoriesRepository }
